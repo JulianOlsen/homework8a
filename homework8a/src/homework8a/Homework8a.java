ad8877be8c3ae167225c62a5d938ea5ce6cdce2c
Julian Olsen <46683927+JulianOlsen@users.noreply.github.com>
2019-06-12 18:34:54 +0200

    Add files via upload

---
+++ homework8a/src/homework8a/Homework8a.java
@@ -0,0 +1,29 @@
+package homework8a;
+
+import java.util.Scanner;
+
+public class Homework8a {
+
+    public static void main(String[] args) {
+        System.out.println("type in 10 digits");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int z = sc.nextInt();
+        int v = sc.nextInt();
+        int t = sc.nextInt();
+        int o = sc.nextInt();
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+        int s = sc.nextInt();
+        int a=0;
+        a=x*y*w*z*v*t*o*p*q*s;
+     
+        
+        System.out.println(a);
+
+        
+        
+    }
+} 
